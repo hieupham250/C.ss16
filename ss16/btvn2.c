ec03ebf7fe5cb7e7a79ac27f9c5df89dd218107e
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2023-12-06 11:03:09 +0700

    Update btvn2.c

--- ss16/btvn2.c
+++ ss16/btvn2.c
@@ -4,6 +4,10 @@ int main(){
 	FILE *file;
 	char str[100];
 	file = fopen("bt01.txt","r");
+	if(file==NULL){
+		printf("Khong the mo file!");
+		return 1;
+	}
 	fread(str,sizeof(char),sizeof(str),file);
 	printf("Noi dung trong file: %s",str);
 	fclose(file);
